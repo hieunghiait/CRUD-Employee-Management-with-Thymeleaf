a7d33631b0e45167aa31ad9ae1d465db9d5a8e4f
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-05-09 16:13:39 +0700

    Implement Update Employee Feature

--- src/main/java/com/example/crudwiththymeleaf/model/Employee.java
+++ src/main/java/com/example/crudwiththymeleaf/model/Employee.java
@@ -2,18 +2,33 @@ package com.example.crudwiththymeleaf.model;
 
 import jakarta.persistence.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Entity
 @Table(name = "employees")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    @NotBlank(message = "Do not empty")
     private long id;
     @Column(name = "first_name")
+    @NotNull
+    @NotBlank(message = "Do not empty")
     private String firstName;
     @Column(name = "last_name")
+    @NotNull
+    @NotBlank(message = "Do not empty")
     private String lastName;
     @Column(name = "email")
+    @NotNull
+    @NotBlank(message = "Do not empty")
     private String email;
+
+    public Employee(){
+
+    }
 
     public Employee(long id, String firstName, String lastName, String email) {
         this.id = id;
