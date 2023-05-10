e381ce219e499abb4986d12d93bbbc3e7d7e2687
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-05-10 18:03:34 +0700

    Sorting col feature

--- src/main/java/com/example/crudwiththymeleaf/controller/EmployeeController.java
+++ src/main/java/com/example/crudwiththymeleaf/controller/EmployeeController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,74 +15,67 @@ import java.util.List;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
-    //display list of employee
+
+    // display list of employees
     @GetMapping("/")
     public String viewHomePage(Model model) {
-        return findPaginated(1, model);
+        return findPaginated(1, "firstName", "asc", model);
     }
 
-    /**
-     * Show list information employee
-     * @param model
-     * @return view
-     */
-
     @GetMapping("/showNewEmployeeForm")
-    public String showEmployeeForm(Model model){
-        //create model attribute to bind form data
+    public String showNewEmployeeForm(Model model) {
+        // create model attribute to bind form data
         Employee employee = new Employee();
         model.addAttribute("employee", employee);
         return "new_employee";
     }
 
-    /**
-     * Save information employee
-     * @param employee
-     * @return chuyển hướng về trang chính
-     */
     @PostMapping("/saveEmployee")
-    public String saveEmployee(@Valid @ModelAttribute("employee") Employee employee){
-        //save employee into database
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        // save employee to database
         employeeService.saveEmployee(employee);
         return "redirect:/";
     }
 
-    /**
-     * Show information employee
-     * @param id
-     * @param model
-     * @return view
-     */
     @GetMapping("/showFormForUpdate/{id}")
-    public String showFormForUpdate(@PathVariable (value = "id") long id, Model model){
-        //get employee from service
-        Employee employee =  employeeService.getEmployeeById(id);
-        //set employee as a model attribute to pre-populate the form
+    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+
+        // get employee from the service
+        Employee employee = employeeService.getEmployeeById(id);
+
+        // set employee as a model attribute to pre-populate the form
         model.addAttribute("employee", employee);
         return "update_employee";
     }
 
-    /**
-     * Delete employee by id
-     * @param id
-     * @param model
-     * @return view HomePage
-     */
     @GetMapping("/deleteEmployee/{id}")
-    public String showFormDelete(@PathVariable (value = "id") long id, Model model){
+    public String deleteEmployee(@PathVariable(value = "id") long id) {
+
+        // call delete employee method
         this.employeeService.deleteEmployeeById(id);
         return "redirect:/";
     }
+
+
     @GetMapping("/page/{pageNo}")
-    public String findPaginated(@PathVariable (value = "pageNo") int pageNo, Model model){
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir,
+                                Model model) {
         int pageSize = 5;
-        Page<Employee> page = employeeService.findPaginated(pageNo, pageSize);
-        List<Employee> listEmployee = page.getContent();
+
+        Page < Employee > page = employeeService.findPaginated(pageNo, pageSize, sortField, sortDir);
+        List < Employee > listEmployees = page.getContent();
 
         model.addAttribute("currentPage", pageNo);
         model.addAttribute("totalPages", page.getTotalPages());
         model.addAttribute("totalItems", page.getTotalElements());
-        model.addAttribute("listEmployees", listEmployee);
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
+        model.addAttribute("listEmployees", listEmployees);
         return "index";
     }
 }
